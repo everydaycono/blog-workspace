042670f7e10438e5a25fe9fbf45175fff9c0b978
Cono <everydaycono@gmail.com>
2023-08-25 09:31:10 +0900

    feat : docker mysql, typeorm setting

---
+++ packages/server/src/config/index.ts
@@ -0,0 +1,5 @@
+import app from './app.config';
+
+export const config = {
+  app,
+};
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,51 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './config';
 
 async function bootstrap() {
+  /*
+  |--------------------------------------------------------------------------
+  | Checks for JWT SECRET, required for app bootstrapping
+  |--------------------------------------------------------------------------
+  */
+  // if (!process.env.JWT_SECRET) {
+  //   throw new Error('Fatal Error. JWT_SECRET variable is not provided');
+  // }
+
+  /*
+  |--------------------------------------------------------------------------
+  | Creates an instance of the NestApplication and set Middlewares
+  |--------------------------------------------------------------------------
+  */
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  //   app.use(helmet.default());
+  //   app.use(rateLimit(config.rateLimit));
+
+  /*
+  |--------------------------------------------------------------------------
+  | Set global filters and pipes
+  |--------------------------------------------------------------------------
+  */
+  //   app.useGlobalFilters(new BadRequestExceptionFilter());
+  //   app.useGlobalPipes(
+  //     new ValidationPipe({
+  //       transform: true,
+  //       whitelist: true,
+  //       forbidNonWhitelisted: true,
+  //       exceptionFactory: (errors) => new BadRequestException(errors),
+  //     }),
+  //   );
+
+  /*
+    |--------------------------------------------------------------------------
+    | Initialize Swagger and APP
+    |--------------------------------------------------------------------------
+    */
+  //   if (process.env.NODE_ENV !== 'production') {
+  //     await bootstrapSwagger(app, config.app);
+  //   }
+  await app.listen(config.app.port);
 }
 bootstrap();
